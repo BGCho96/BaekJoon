053447a30ce8e8f7de42c52696fdb09af6b89af6
BGCho96 <73811730+BGCho96@users.noreply.github.com>
2024-11-12 18:36:05 +0900

    squareQuestion
    square Q in progress. some notes added

---
+++ findSquare/findSquare.py
@@ -0,0 +1,19 @@
+N, M = map(int,input().split())
+
+box = []
+for n in range(N):
+    box.append(list(map(int,input())))
+numbers=[i for i in range(0,N*M)]
+print(box)
+# while numbers:
+#     startPoint=numbers.pop()
+#     for endpoint in numbers:
+        
+        
+
+
+
+
+
+
+
