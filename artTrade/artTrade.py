ca1e3df8f59d1804d85688d562e16c6fd59530d2
BGCho96 <73811730+BGCho96@users.noreply.github.com>
2025-01-06 15:28:27 +0900

    artTrade progress update
    problem pregress and notes added

---
+++ artTrade/artTrade.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+answer=1
+N = int(input())
+priceInfo = []
+for i in range(N):
+    temp=list(map(int,input().strip()))
+    priceInfo.append(temp)
+
+# print(priceInfo)
+tradeTrace=[[1,0]]
+while tradeTrace:
+    newTrace=[]
+    for sellerInfo in tradeTrace:
+        sellerList = sellerInfo[:-1]
+        price = sellerInfo[-1]
+        lastPossess = sellerList[-1]
+        for buyer in range(1,len(priceInfo[lastPossess-1])+1):
+            newPirce = priceInfo[lastPossess-1][buyer-1]
+            if newPirce>=price and buyer not in sellerList:
+                newList = sellerList[:]
+                newList.append(buyer)
+                if len(newList) > answer:
+                    answer = len(newList)
+                newList.append(newPirce)
+                newTrace.append(newList)
+    tradeTrace=newTrace
+print(answer)
--- decreasingDigit/decreasingDigit.py
+++ decreasingDigit/decreasingDigit.py
@@ -20,69 +20,69 @@ def trueCombination(l,r):
         l = l - 1
         answer/=div
     return answer
-SN = int(input())
-for N in range(SN):
-    answerSack=[]
+N = int(input())
+
+answerSack=[]
+digit = 0
+for i in range(1,12):
+    digit+=1
+    combination = digitCombination(10,i)
+    # print(N, combination)
+    if N >= combination:
+        N -= combination
+    else:break
+# print(N)
+# digit-=1
+maxDigitStart = digit-1
+firstDigit = 1
+answer=0
+if digit >= 11:
+    digit = 0 
+    answer=-1
+if int(N) == 0:
+    # if digit == 9:
+    #     digit+=1
+    for i in range(int(digit)-1,-1,-1):
+                answerSack.append(i)
+    # print(''.join(map(str,answerSack)))
     digit = 0
-    for i in range(1,12):
-        digit+=1
-        combination = digitCombination(10,i)
-        # print(N, combination)
-        if N >= combination:
-            N -= combination
-        else:break
-    # print(N)
-    # digit-=1
-    maxDigitStart = digit-1
-    firstDigit = 1
-    answer=0
-    if digit >= 11:
-        digit = 0 
-        answer=-1
-    if int(N) == 0:
-        # if digit == 9:
-        #     digit+=1
-        for i in range(int(digit)-1,-1,-1):
-                    answerSack.append(i)
-        # print(''.join(map(str,answerSack)))
-        digit = 0
 
-    while digit:
-        #Digit 자릿수가 있는 것은 자명. 제일 큰 자릿수는 1, 나머지는 0부터 늘려가며 좁히기.
-        # if firstDigit:
-        #     combination = digitCombination(maxDigitStart+1,digit)
-        # else:
-        combination = trueCombination(maxDigitStart+1,digit)
-        if combination <= N : # 조합 부족, 더 큰 수 시도
-            maxDigitStart+=1
-            if maxDigitStart>9:
-                answer=-1
-                break
-        else: # 이 수가 맞음. 고정시키고 아랫자리로 이동
-            
-            # subCombination = trueCombination(maxDigitStart,digit)
-            if answerSack and (maxDigitStart == answerSack[-1]): # 올리는 경우...?
-                answerSack[-1]+=1
-                maxDigitStart=0
-            answerSack.append(maxDigitStart)
-            firstDigit = 0
-            
-            subCombination = trueCombination(maxDigitStart,digit)
-            digit-=1
-            N-=subCombination
-            maxDigitStart = 0
-            
-
+while digit:
+    #Digit 자릿수가 있는 것은 자명. 제일 큰 자릿수는 1, 나머지는 0부터 늘려가며 좁히기.
+    # if firstDigit:
+    #     combination = digitCombination(maxDigitStart+1,digit)
+    # else:
+    combination = trueCombination(maxDigitStart+1,digit)
+    if combination <= N : # 조합 부족, 더 큰 수 시도
+        maxDigitStart+=1
+        if maxDigitStart>9:
+            answer=-1
+            break
+    else: # 이 수가 맞음. 고정시키고 아랫자리로 이동
+        
+        # subCombination = trueCombination(maxDigitStart,digit)
+        if answerSack and (maxDigitStart == answerSack[-1]): # 올리는 경우...?
+            answerSack[-1]+=1
+            maxDigitStart=0
+        answerSack.append(maxDigitStart)
+        firstDigit = 0
+        
+        subCombination = trueCombination(maxDigitStart,digit)
+        digit-=1
+        N-=subCombination
+        maxDigitStart = 0
         
 
+    
 
-            
-            
-            
-    # print(answerSack)
-    if answer == 0:
-        print(''.join(map(str,answerSack)))
-    else: print(answer)
 
         
+        
+        
+# print(answerSack)
+if answer == 0:
+    print(''.join(map(str,answerSack)))
+else: print(answer)
+
+    
 
